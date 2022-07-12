7365ba230e8d1106a568a46cab241987b8e4af62
Evgeny Abramovich <evg.abramovitch@gmail.com>
2022-07-12 12:29:05 +0300

    Injected http client as dependency

--- internal/proxy/options.go
+++ internal/proxy/options.go
@@ -1,14 +1,22 @@
 package proxy
 
+import "net/http"
+
 type Replcaer interface {
 	ToTarget(targetUrl string) (string, error)
 	ToSource(targetUrl string, host string) (string, error)
 }
 
-type ProxyMiddlewareOptions = func(*ProxyMiddleware)
+type proxyMiddlewareOptions = func(*ProxyMiddleware)
 
-func WithUrlReplcaer(replcaer Replcaer) ProxyMiddlewareOptions {
+func WithUrlReplcaer(replcaer Replcaer) proxyMiddlewareOptions {
 	return func(pm *ProxyMiddleware) {
 		pm.replcaer = replcaer
+	}
+}
+
+func WithHttpClient(http http.Client) proxyMiddlewareOptions {
+	return func(pm *ProxyMiddleware) {
+		pm.http = http
 	}
 }
