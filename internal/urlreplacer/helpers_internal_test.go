bf79f62ff086e6001b094814c08c8cdae54acf49
Evgeny Abramovich <evg.abramovitch@gmail.com>
2022-10-08 22:01:32 +0300

    Added tests for wildCardToReplacePattern

--- internal/urlreplacer/helpers_internal_test.go
+++ internal/urlreplacer/helpers_internal_test.go
@@ -9,64 +9,76 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var testCases = []struct {
+	name            string
+	url             string
+	expectedRegexp  string
+	expectedPattern string
+}{
+	{
+		name:            "localhost",
+		url:             "localhost",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?localhost(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}localhost${path}",
+	},
+	{
+		name:            "localhost with port",
+		url:             "localhost:3000",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?localhost:3000(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}localhost:3000${path}",
+	},
+	{
+		name:            "single star",
+		url:             "*",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}${part1}${path}",
+	},
+	{
+		name:            "single star with port",
+		url:             "*:3001",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+):3001(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}${part1}:3001${path}",
+	},
+	{
+		name:            "single star with url part",
+		url:             "demo.*.com",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?demo\.(?P<part1>.+)\.com(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}demo.${part1}.com${path}",
+	},
+	{
+		name:            "single star with url part and port",
+		url:             "api.*.com:3001",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?api\.(?P<part1>.+)\.com:3001(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}api.${part1}.com:3001${path}",
+	},
+	{
+		name:            "multiple stars with url part",
+		url:             "*.host.*.com",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.host\.(?P<part2>.+)\.com(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}${part1}.host.${part2}.com${path}",
+	},
+	{
+		name:            "multiple stars with url part and port",
+		url:             "*.host.*.com:3001",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.host\.(?P<part2>.+)\.com:3001(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}${part1}.host.${part2}.com:3001${path}",
+	},
+	{
+		name:            "host with default http port",
+		url:             "*.api.com:80",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.api\.com(:80)?(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}${part1}.api.com:80${path}",
+	},
+	{
+		name:            "host with default https port",
+		url:             "*.api.com:443",
+		expectedRegexp:  `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.api\.com(:443)?(?P<path>[\/?].*)?$`,
+		expectedPattern: "${scheme}${part1}.api.com:443${path}",
+	},
+}
+
 func TestWildCardToRegexp(t *testing.T) {
 	t.Run("regexp", func(t *testing.T) {
-		testCases := []struct {
-			name     string
-			url      string
-			expected string
-		}{
-			{
-				name:     "localhost",
-				url:      "localhost",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?localhost(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "localhost with port",
-				url:      "localhost:3000",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?localhost:3000(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "single star",
-				url:      "*",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "single star with port",
-				url:      "*:3001",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+):3001(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "single star with url part",
-				url:      "demo.*.com",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?demo\.(?P<part1>.+)\.com(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "single star with url part and port",
-				url:      "api.*.com:3001",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?api\.(?P<part1>.+)\.com:3001(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "multiple stars with url part",
-				url:      "*.host.*.com",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.host\.(?P<part2>.+)\.com(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "multiple stars with url part and port",
-				url:      "*.host.*.com:3001",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.host\.(?P<part2>.+)\.com:3001(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "host with default http port",
-				url:      "*.api.com:80",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.api\.com(:80)?(?P<path>[\/?].*)?$`,
-			},
-			{
-				name:     "host with default https port",
-				url:      "*.api.com:443",
-				expected: `^(?P<scheme>(http(s?):)?\/\/)?(?P<part1>.+)\.api\.com(:443)?(?P<path>[\/?].*)?$`,
-			},
-		}
 		for _, testCase := range testCases {
 			t.Run(testCase.name, func(t *testing.T) {
 				parsedPattern, err := urlx.Parse(testCase.url)
@@ -75,7 +87,7 @@ func TestWildCardToRegexp(t *testing.T) {
 				regexp, _, err := wildCardToRegexp(parsedPattern)
 
 				assert.NoError(t, err)
-				assert.Equal(t, testCase.expected, regexp.String())
+				assert.Equal(t, testCase.expectedRegexp, regexp.String())
 			})
 		}
 	})
@@ -137,6 +149,62 @@ func TestWildCardToRegexp(t *testing.T) {
 				_, _, err := wildCardToRegexp(&testCase.parsedPattern)
 
 				assert.EqualError(t, err, testCase.expected)
+			})
+		}
+	})
+}
+
+func TestWildCardToReplacePattern(t *testing.T) {
+	t.Run("pattern", func(t *testing.T) {
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				parsedPattern, err := urlx.Parse(testCase.url)
+				testutils.CheckNoError(t, err)
+
+				pattern, _, err := wildCardToReplacePattern(parsedPattern)
+
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.expectedPattern, pattern)
+			})
+		}
+	})
+
+	t.Run("wildcard count", func(t *testing.T) {
+		testCases := []struct {
+			name     string
+			url      string
+			expected int
+		}{
+			{
+				name:     "no wildcards",
+				url:      "localhost",
+				expected: 0,
+			},
+			{
+				name:     "single star",
+				url:      "*",
+				expected: 1,
+			},
+			{
+				name:     "two stars",
+				url:      "*.*.com",
+				expected: 2,
+			},
+			{
+				name:     "three stars",
+				url:      "api.*.*.*.com",
+				expected: 3,
+			},
+		}
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				parsedPattern, err := urlx.Parse(testCase.url)
+				testutils.CheckNoError(t, err)
+
+				_, count, err := wildCardToReplacePattern(parsedPattern)
+
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.expected, count)
 			})
 		}
 	})
