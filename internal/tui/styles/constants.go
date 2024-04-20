fa891450febda6f0db7123327b287ae5de0f989e
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-04-20 19:12:28 -0300

    Updated prefixes

--- internal/tui/styles/constants.go
+++ internal/tui/styles/constants.go
@@ -1,15 +1,9 @@
 package styles
 
-import (
-	"strings"
-
-	"github.com/charmbracelet/log"
-)
-
 var (
-	DebugLabel   = strings.ToUpper(log.DebugLevel.String())
-	InfoLabel    = strings.ToUpper(log.InfoLevel.String())
-	WarningLabel = strings.ToUpper(log.WarnLevel.String())
-	ErrorLabel   = strings.ToUpper(log.ErrorLevel.String())
-	FatalLabel   = strings.ToUpper(log.FatalLevel.String())
+	DebugLabel   = "DEBUG"
+	InfoLabel    = "INFO "
+	WarningLabel = "WARN "
+	ErrorLabel   = "ERROR"
+	FatalLabel   = "FATAL"
 )
