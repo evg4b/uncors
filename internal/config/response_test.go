386e5ad9e41254694ad5053204797e16a56bb12e
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-06-30 20:07:32 -0300

    Added test for response

--- internal/config/response_test.go
+++ internal/config/response_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/evg4b/uncors/pkg/fakedata"
+
 	"github.com/evg4b/uncors/internal/config"
 	"github.com/go-http-utils/headers"
 	"github.com/stretchr/testify/assert"
@@ -34,5 +36,64 @@ func TestResponseClone(t *testing.T) {
 
 	t.Run("not same Headers map", func(t *testing.T) {
 		assert.NotSame(t, &response.Headers, &clonedResponse.Headers)
+	})
+
+	t.Run("response type", func(t *testing.T) {
+		t.Run("raw response", func(t *testing.T) {
+			response := config.Response{
+				Code: http.StatusOK,
+				Raw:  "this is plain text",
+			}
+
+			t.Run("IsRaw", func(t *testing.T) {
+				assert.True(t, response.IsRaw())
+			})
+
+			t.Run("IsFile", func(t *testing.T) {
+				assert.False(t, response.IsFile())
+			})
+
+			t.Run("IsFake", func(t *testing.T) {
+				assert.False(t, response.IsFake())
+			})
+		})
+
+		t.Run("file response", func(t *testing.T) {
+			response := config.Response{
+				Code: http.StatusOK,
+				File: "~/projects/uncors/response/demo.json",
+			}
+
+			t.Run("IsRaw", func(t *testing.T) {
+				assert.False(t, response.IsRaw())
+			})
+
+			t.Run("IsFile", func(t *testing.T) {
+				assert.True(t, response.IsFile())
+			})
+
+			t.Run("IsFake", func(t *testing.T) {
+				assert.False(t, response.IsFake())
+			})
+		})
+
+		t.Run("file response", func(t *testing.T) {
+			response := config.Response{
+				Code: http.StatusOK,
+				Fake: &fakedata.Node{Type: "sentence"},
+			}
+
+			t.Run("IsRaw", func(t *testing.T) {
+				assert.False(t, response.IsRaw())
+			})
+
+			t.Run("IsFile", func(t *testing.T) {
+				assert.False(t, response.IsFile())
+			})
+
+			t.Run("IsFake", func(t *testing.T) {
+				assert.True(t, response.IsFake())
+			})
+		})
 	})
 }
