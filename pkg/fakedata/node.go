7c19075c8e72618f62ca9b7a1c846232ff585dad
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-06-30 15:19:12 -0300

    Fixed seeding

--- pkg/fakedata/node.go
+++ pkg/fakedata/node.go
@@ -1,5 +1,9 @@
 package fakedata
 
+import (
+	"github.com/evg4b/uncors/internal/helpers"
+)
+
 type Node struct {
 	Type       string          `mapstructure:"type"`
 	Item       *Node           `mapstructure:"item"`
@@ -7,4 +11,15 @@ type Node struct {
 	Options    map[string]any  `mapstructure:"options"`
 	Count      int             `mapstructure:"count"`
 	Seed       uint64          `mapstructure:"seed"`
+}
+
+func (root *Node) Clone() *Node {
+	return &Node{
+		Type:       root.Type,
+		Item:       root.Item,
+		Properties: helpers.CloneMap(root.Properties),
+		Options:    helpers.CloneMap(root.Options),
+		Count:      root.Count,
+		Seed:       root.Seed,
+	}
 }
