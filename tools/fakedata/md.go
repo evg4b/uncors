5667c6f565a392a9c8299beff38cd1306c64dd31
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-09-22 13:04:00 -0300

    Added groups to the generator

--- tools/fakedata/md.go
+++ tools/fakedata/md.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strings"
 
+	"github.com/charmbracelet/log"
+
 	"github.com/samber/lo"
 
 	"github.com/brianvoe/gofakeit/v7"
@@ -12,23 +14,57 @@ import (
 )
 
 type MdTableRow struct {
-	Type        []string
-	Description []string
+	Type        string
+	Description string
 	Params      []string
-	Example     []string
+	Example     string
+	Group       string
+}
+
+var groups = map[string][]string{
+	"Numbers": {
+		"number",
+		"int",
+		"intn",
+		"int8",
+		"int16",
+		"int32",
+		"int64",
+		"uint",
+		"uintn",
+		"uint8",
+		"uint16",
+		"uint32",
+		"uint64",
+		"float32",
+		"float32range",
+		"float64",
+		"float64range",
+	},
 }
 
 func generateMdData() {
 	rows := make([]MdTableRow, 0)
+	groupsMap := make(map[string]string)
+	lo.ForEach(lo.Keys(groups), func(group string, _ int) {
+		lo.ForEach(groups[group], func(item string, _ int) {
+			groupsMap[item] = group
+		})
+	})
+
 	for _, typeKey := range fakedata.GetTypes() {
 		info := gofakeit.GetFuncLookup(typeKey)
 		if info == nil {
+			log.Warnf("Type %s not found in fakedata", typeKey)
+
 			continue
 		}
+
 		rows = append(rows, MdTableRow{
-			Type:        []string{typeKey},
-			Description: []string{info.Description},
-			Example:     []string{info.Example},
+			Type:        typeKey,
+			Description: info.Description,
+			Example:     info.Example,
+			Group:       groupsMap[typeKey],
 			Params: lo.Map(info.Params, func(param gofakeit.Param, _ int) string {
 				return fmt.Sprintf("%s (%s) - %s", param.Field, param.Type, param.Description)
 			}),
@@ -40,28 +76,49 @@ func generateMdData() {
 		panic(err)
 	}
 
-	if _, err = mdFile.WriteString("| Type | Description | Params | Example |\n"); err != nil {
-		panic(err)
-	}
+	groupedData := lo.GroupBy(rows, func(item MdTableRow) string {
+		return item.Group
+	})
 
-	if _, err = mdFile.WriteString("| ---- | ----------- | ------- | ------- |\n"); err != nil {
-		panic(err)
-	}
-
-	for _, row := range rows {
-		if _, err = fmt.Fprintf(
-			mdFile,
-			"| %s | %s | %s | %s |\n",
-			f(row.Type),
-			f(row.Description),
-			f(row.Params),
-			f(row.Example),
-		); err != nil {
+	lo.ForEach(lo.Keys(groupedData), func(item string, _ int) {
+		rows := groupedData[item]
+		if _, err = fmt.Fprintf(mdFile, "### %s\n", item); err != nil {
 			panic(err)
 		}
-	}
+
+		if _, err = mdFile.WriteString("| Type | Description | Params | Example |\n"); err != nil {
+			panic(err)
+		}
+
+		if _, err = mdFile.WriteString("| ---- | ----------- | ------- | ------- |\n"); err != nil {
+			panic(err)
+		}
+
+		for _, row := range rows {
+			if _, err = fmt.Fprintf(
+				mdFile,
+				"| %s | %s | %s | %s |\n",
+				f(row.Type),
+				f(row.Description),
+				li(row.Params),
+				f(row.Example),
+			); err != nil {
+				panic(err)
+			}
+		}
+
+		if _, err = mdFile.WriteString("\n\n"); err != nil {
+			panic(err)
+		}
+	})
 }
 
-func f(lines []string) string {
-	return strings.ReplaceAll(strings.Join(lines, "\n"), "\n", "<br>")
+func f(lines string) string {
+	return strings.ReplaceAll(lines, "\n", "<br>")
+}
+
+func li(lines []string) string {
+	return strings.Join(lo.Map(lines, func(item string, _ int) string {
+		return strings.ReplaceAll(item, "\n", ".")
+	}), "<br>")
 }
