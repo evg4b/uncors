882ee9b38736d15d083ea87a5cc0af8ba842ba93
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-03-22 13:30:11 -0300

    Added snapshot testing

--- internal/tui/box-message_test.go
+++ internal/tui/box-message_test.go
@@ -5,30 +5,23 @@ import (
 	"testing"
 
 	"github.com/evg4b/uncors/testing/testutils"
+	"github.com/gkampitakis/go-snaps/snaps"
 
 	"github.com/evg4b/uncors/internal/tui"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPrintWarningBox(t *testing.T) {
 	tests := []struct {
-		name     string
-		message  string
-		expected string
+		name    string
+		message string
 	}{
 		{
 			name:    "render single line message",
 			message: "test message",
-			expected: "\x1b[48;2;255;211;0m \x1b[0m\x1b[38;2;0;0;0;48;2;255;211;" +
-				"0mWARN \x1b[0m\x1b[48;2;255;211;0m \x1b[0m test message\n",
 		},
 		{
 			name:    "render multi line message",
 			message: "test message\nsecond line",
-			expected: "\x1b[48;2;255;211;0m \x1b[0m\x1b[38;2;0;0;0;48;2;255;211;" +
-				"0mWARN \x1b[0m\x1b[48;2;255;211;0m \x1b[0m test message\n\x1b[48;2;255" +
-				";211;0m \x1b[0m\x1b[38;2;0;0;0;48;2;255;211;0m\x1b[0m\x1b[48;2;255;" +
-				"211;0m \x1b[0m\x1b[48;2;255;211;0m     \x1b[0m second line \n",
 		},
 	}
 	for _, testCase := range tests {
@@ -37,30 +30,23 @@ func TestPrintWarningBox(t *testing.T) {
 
 			tui.PrintWarningBox(&buffer, testCase.message)
 
-			assert.Equal(t, testCase.expected, buffer.String())
+			snaps.MatchSnapshot(t, buffer.String())
 		}))
 	}
 }
 
 func TestPrintInfoBox(t *testing.T) {
 	tests := []struct {
-		name     string
-		message  string
-		expected string
+		name    string
+		message string
 	}{
 		{
 			name:    "render single line message",
 			message: "test message",
-			expected: "\x1b[48;2;0;113;206m \x1b[0m\x1b[38;2;0;0;0;48;2;0;113;" +
-				"206mINFO \x1b[0m\x1b[48;2;0;113;206m \x1b[0m test message\n",
 		},
 		{
 			name:    "render multi line message",
 			message: "test message\nsecond line",
-			expected: "\x1b[48;2;0;113;206m \x1b[0m\x1b[38;2;0;0;0;48;2;0;113;" +
-				"206mINFO \x1b[0m\x1b[48;2;0;113;206m \x1b[0m test message\n\x1b[48;2;0;" +
-				"113;206m \x1b[0m\x1b[38;2;0;0;0;48;2;0;113;206m\x1b[0m\x1b[48;2;0;113;" +
-				"206m \x1b[0m\x1b[48;2;0;113;206m     \x1b[0m second line \n",
 		},
 	}
 	for _, testCase := range tests {
@@ -69,7 +55,7 @@ func TestPrintInfoBox(t *testing.T) {
 
 			tui.PrintInfoBox(&buffer, testCase.message)
 
-			assert.Equal(t, testCase.expected, buffer.String())
+			snaps.MatchSnapshot(t, buffer.String())
 		}))
 	}
 }
--- internal/uncors/loggers_test.go
+++ internal/uncors/loggers_test.go
@@ -5,10 +5,9 @@ import (
 	"testing"
 
 	"github.com/charmbracelet/log"
-	"github.com/evg4b/uncors/internal/tui/styles"
 	"github.com/evg4b/uncors/internal/uncors"
 	"github.com/evg4b/uncors/testing/testutils"
-	"github.com/stretchr/testify/assert"
+	"github.com/gkampitakis/go-snaps/snaps"
 )
 
 func TestLoggers(t *testing.T) {
@@ -16,29 +15,24 @@ func TestLoggers(t *testing.T) {
 		log.SetLevel(log.DebugLevel)
 
 		tests := []struct {
-			name     string
-			logger   *log.Logger
-			expected string
+			name   string
+			logger *log.Logger
 		}{
 			{
-				name:     "ProxyLogger",
-				logger:   uncors.NewProxyLogger(log.Default()),
-				expected: styles.ProxyStyle.Render("PROXY"),
+				name:   "ProxyLogger",
+				logger: uncors.NewProxyLogger(log.Default()),
 			},
 			{
-				name:     "MockLogger",
-				logger:   uncors.NewMockLogger(log.Default()),
-				expected: styles.MockStyle.Render("MOCK"),
+				name:   "MockLogger",
+				logger: uncors.NewMockLogger(log.Default()),
 			},
 			{
-				name:     "StaticLogger",
-				logger:   uncors.NewStaticLogger(log.Default()),
-				expected: styles.StaticStyle.Render("STATIC"),
+				name:   "StaticLogger",
+				logger: uncors.NewStaticLogger(log.Default()),
 			},
 			{
-				name:     "CacheLogger",
-				logger:   uncors.NewCacheLogger(log.Default()),
-				expected: styles.CacheStyle.Render("CACHE"),
+				name:   "CacheLogger",
+				logger: uncors.NewCacheLogger(log.Default()),
 			},
 		}
 		for _, testCase := range tests {
@@ -46,31 +40,31 @@ func TestLoggers(t *testing.T) {
 				t.Run("Error", testutils.UniqOutput(output, func(t *testing.T, output *bytes.Buffer) {
 					testCase.logger.Error("error")
 
-					assert.Contains(t, output.String(), testCase.expected)
+					snaps.MatchSnapshot(t, output.String())
 				}))
 
 				t.Run("Warn", testutils.UniqOutput(output, func(t *testing.T, output *bytes.Buffer) {
 					testCase.logger.Warn("warn")
 
-					assert.Contains(t, output.String(), testCase.expected)
+					snaps.MatchSnapshot(t, output.String())
 				}))
 
 				t.Run("Info", testutils.UniqOutput(output, func(t *testing.T, output *bytes.Buffer) {
 					testCase.logger.Info("info")
 
-					assert.Contains(t, output.String(), testCase.expected)
+					snaps.MatchSnapshot(t, output.String())
 				}))
 
 				t.Run("Debug", testutils.UniqOutput(output, func(t *testing.T, output *bytes.Buffer) {
 					testCase.logger.Debug("debug")
 
-					assert.Contains(t, output.String(), testCase.expected)
+					snaps.MatchSnapshot(t, output.String())
 				}))
 
 				t.Run("Print", testutils.UniqOutput(output, func(t *testing.T, output *bytes.Buffer) {
 					testCase.logger.Print("print")
 
-					assert.Contains(t, output.String(), testCase.expected)
+					snaps.MatchSnapshot(t, output.String())
 				}))
 			})
 		}
