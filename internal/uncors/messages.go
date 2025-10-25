6b11d387d1c58fff8a8c41ae7712fdba714098a9
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-10-25 09:29:41 -0400

    Improve new version available message
    - Fixed capitalization: 'Available' to 'AVAILABLE'
    - Improved grammar: 'is not latest' to 'is not the latest version'
    - Changed 'on' to 'at' for URL reference

--- internal/uncors/messages.go
+++ internal/uncors/messages.go
@@ -4,7 +4,7 @@ const DisclaimerMessage = `DON'T USE IT FOR PRODUCTION!
 This is a reverse proxy for use in testing or debugging web applications locally.
 It hasn't been reviewed for security issues.`
 
-const NewVersionIsAvailable = `NEW VERSION IS Available!
-%s is not latest, you should upgrade to %s.
-See more information on https://github.com/evg4b/uncors/releases
+const NewVersionIsAvailable = `NEW VERSION IS AVAILABLE!
+%s is not the latest version, you should upgrade to %s.
+See more information at https://github.com/evg4b/uncors/releases
 `
